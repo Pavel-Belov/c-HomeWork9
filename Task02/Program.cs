43761f1eaed3364f0a4ab90be96ea11549ddfa4f
Pavel Belov <job.pb@yandex.ru>
2023-02-10 22:58:47 +0300

    add home work 9 task02

---
+++ Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void NaturalNums(int m, int n)
+{
+    if (n < m) Console.Write($"");
+    else
+    {
+        NaturalNums(m, n - 1);
+        Console.Write($"{n} ");
+    }
+}
+
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (m > 0 && n > 0)
+{
+    if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    System.Console.WriteLine($"Натуральные числа от {m} до {n}:");
+    NaturalNums(m, n);
+}
+else Console.WriteLine("Ошибка. Натуральным является число больше нуля!");
