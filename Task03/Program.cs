f888c562f9af6b4ea557309dc536a6c6d4a77ca9
Pavel Belov <job.pb@yandex.ru>
2023-02-10 23:17:35 +0300

    add home work 9 task03

---
+++ Task03/Program.cs
@@ -0,0 +1,34 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+void PrintElements(int m, int n)
+{
+    if (n < m) Console.Write($"");
+    else
+    {
+        PrintElements(m, n - 1);
+        Console.Write($"{n} ");
+    }
+}
+
+double SumElements(int m, int n)
+{
+    if (n == m) return n;
+    else return n + SumElements(m, n - 1);
+}
+
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (m > n)
+{
+    int temp = m;
+    m = n;
+    n = temp;
+}
+
+System.Console.WriteLine($"Получаем последовательность чисел от {m} до {n}: ");
+PrintElements(m, n);
+Console.WriteLine();  
+System.Console.Write($"Сумма чисел от {m} до {n} равна {SumElements(m, n)}");
