86d2fc1d3b45174d1b5ee1d8e1ce5fbef3d5a1b4
Pavel Belov <job.pb@yandex.ru>
2023-02-10 22:37:43 +0300

    add home work 9 task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void NaturalNums(int n)
+{
+    if (n == 1) Console.Write("");
+    else NaturalNums(n - 1);
+    Console.Write($"{n} ");
+}
+
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0) NaturalNums(n);
+else Console.WriteLine("Ошибка. Натуральным является число больше нуля!");
